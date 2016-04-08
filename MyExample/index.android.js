7b6faad363e832645aaacf049dc7347d71002541
aboutqx <1004945442@qq.com>
2016-04-08 18:26:53 +0800

    move

---
+++ MyExample/index.android.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ */
+'use strict';
+
+var React = require('react-native');
+import Root from './app/root';
+
+const {
+  AppRegistry,
+} = React;
+
+AppRegistry.registerComponent('MyExample', () => Root);
+
+
+
+
